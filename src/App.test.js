c889b91ce1cfb763360c738823d7ac855a7e8372
Giuliano1911 <giulianotorrisi1911@yahoo.com>
2024-12-05 16:56:54 +0100

    !

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react'
-import App from './App'
+import '@testing-library/jest-dom'
+import Welcome from './components/Welcome'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+describe('title', () => {
+  it('is present at the start', () => {
+    render(<Welcome />)
+    const title = screen.getByText(/Bookshop/i)
+    expect(title).toBeInTheDocument()
+  })
 })
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,15 +1,12 @@
 import { Container } from 'react-bootstrap'
-import { Component } from 'react'
 
-class Welcome extends Component {
-  render() {
-    return (
-      <Container className="text-white d-flex align-items-center flex-column mt-5">
-        <h1>Bookshop</h1>
-        <h5>Compra Libri</h5>
-      </Container>
-    )
-  }
+const Welcome = () => {
+  return (
+    <Container className="text-white d-flex align-items-center flex-column mt-5">
+      <h1>Bookshop</h1>
+      <h5>Compra Libri</h5>
+    </Container>
+  )
 }
 
 export default Welcome
